5fcf851d85ecd6a5ba4231c94e952e13e8335677
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-03 13:56:29 -0600

    RESCountry
    Create OceaniaCountriesRetriever class

---
+++ M02/Assignment/app/src/main/java/com/example/rescountry/retrofeit/OceaniaCountryReceiver.kt
@@ -0,0 +1,33 @@
+package com.example.rescountry.retrofeit
+
+import com.example.rescountry.interfaces.OceaniaCountriesAPI
+import com.example.rescountry.models.Countries
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class OceaniaCountriesReceiver {
+    companion object{
+        const val BASE_URL = "https://restcountries.eu/#api-endpoints-all/"
+    }
+
+
+    // TODO 7 create a fun to parse the list
+    fun getCountries(): Call<Countries.OceaniaCountryList> {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        val countriesAPI = retrofit.create(OceaniaCountriesAPI::class.java)
+
+        return countriesAPI.getCountries()
+
+
+    }
+}
