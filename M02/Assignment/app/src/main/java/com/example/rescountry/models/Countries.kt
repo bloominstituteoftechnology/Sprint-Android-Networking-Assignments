6bb624f6727b9c49aec95526fef67de7f59079db
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-03 13:48:15 -0600

    RESCountry
    create Model class

---
+++ M02/Assignment/app/src/main/java/com/example/rescountry/models/Countries.kt
@@ -0,0 +1,22 @@
+package com.example.rescountry.models
+
+//TODO 1 create countries model
+
+// TODO 2 create an initial val for country and make an empty list for countries
+
+
+class Countries{
+
+    data class OceaniaCountryList(val country: List<OceaniaCountry>) {
+
+        // TODO 3 create an OceaniaCountry class to hold country information
+        data class OceaniaCountry(
+            val alpha2code: String,
+            val capital: String,
+            val subregion: String
+        )
+
+
+    }
+
+}
