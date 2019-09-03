47ef98b3d8ffb74c08fd56e6d3e2b4b4c082d808
gcgsauce <gattigcg@gmail.com>
2019-09-04 05:09:41 +0800

    done

--- M01_Threading/Assignment/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ M01_Threading/Assignment/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,8 @@ import java.lang.Math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
+    private val task = MyAsyncTask()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,18 +22,18 @@ class MainActivity : AppCompatActivity() {
 //        prime_number_text.visibility = View.VISIBLE
 
         toggleProgressBar()
-        val task = MyAsyncTask()
+
         task.execute()
     }
 
-//    fun primes(): Sequence<Long> {
-//        var i = 0L
-//        return sequence {
-//            generateSequence { i++ }
-//                .filter { n -> n > 1 && ((2 until sqrt(n.toDouble()).toInt()).none { i -> n % i == 0L }) }
-//                .forEach { yield(it) }
-//        }
-//    }
+    fun primes(): Sequence<Long> {
+        var i = 0L
+        return sequence {
+            generateSequence { i++ }
+                .filter { n -> n > 1 && ((2 until sqrt(n.toDouble()).toInt()).none { i -> n % i == 0L }) }
+                .forEach { yield(it) }
+        }
+    }
 
     fun toggleProgressBar() {
         if (progressBar.visibility == View.VISIBLE) {
@@ -41,8 +43,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        task.cancel(true)
+    }
+
     //params(pass into doInBackground), progress(use to update the progress), result (passed into the post execute from doInBackground)
-    private class MyAsyncTask : AsyncTask<String, Int, String>() {
+    private inner class MyAsyncTask : AsyncTask<String, Int, String>() {
 
         override fun onProgressUpdate(vararg values: Int?) {
             super.onProgressUpdate(*values)
@@ -57,12 +64,7 @@ class MainActivity : AppCompatActivity() {
 
         override fun doInBackground(vararg p: String?): String {
             var i = 0L
-            return sequence {
-                generateSequence { i++ }
-                    .filter { n -> n > 1 && ((2 until sqrt(n.toDouble()).toInt()).none { i -> n % i == 0L }) }
-                    .forEach { yield(it) }
-                onProgressUpdate(n)
-            }.take(1600).joinToString(", ")
+            return primes().take(1600).joinToString("")
         }
 
         override fun onCancelled(result: String?) {
