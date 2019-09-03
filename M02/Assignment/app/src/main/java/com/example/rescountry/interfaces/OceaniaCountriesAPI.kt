57e573d261cf9dd0676c94d3e48caff662b5a2a6
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-03 13:51:18 -0600

    RESCountry
    Create OceaniaCountriesAPI interface

---
+++ M02/Assignment/app/src/main/java/com/example/rescountry/interfaces/OceaniaCountriesAPI.kt
@@ -0,0 +1,18 @@
+package com.example.rescountry.interfaces
+
+import com.example.rescountry.models.Countries
+import retrofit2.Call
+import retrofit2.http.GET
+
+//TODO 4 create an interface for API Url and returns the results for CountryList List model
+
+interface OceaniaCountriesAPI {
+
+    //TODO 5 get the last position of the url
+
+    @GET("<https://restcountries.eu/#api-endpoints-all/>")
+    //then create a function to call the api from the web and populate the list
+    fun getCountries(): Call<Countries.OceaniaCountryList>
+}
+
+
