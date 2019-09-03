4c63533a819e07c605f4f31fc280db81d76a2e49
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-03 16:26:44 -0600

    REST Country
    update endpoint url

--- M02/Assignment/app/src/main/java/com/example/rescountry/retrofeit/OceaniaCountryReceiver.kt
+++ M02/Assignment/app/src/main/java/com/example/rescountry/retrofeit/OceaniaCountryReceiver.kt
@@ -11,7 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class OceaniaCountriesReceiver {
     companion object{
-        const val BASE_URL = "https://restcountries.eu/#api-endpoints-all"
+        const val BASE_URL = "https://restcountries.eu/"
     }
 
 
